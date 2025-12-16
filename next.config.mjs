cc9ce10c5a9261b2314d157284252a9f5518d975
stack-Tech-1 <stacktechnologies0@gmail.com>
2025-12-16 16:37:36 +0100

    oage

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // or 'export' for static sites
+  trailingSlash: true, // Try this for Amplify routing
   /* config options here */
   reactStrictMode: true,
 };
